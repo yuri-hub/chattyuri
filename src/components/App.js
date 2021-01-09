b331292559a699ee0320ee85f1c236ca4435ed7c
Yuri Saito <yuri@Yuris-MacBook-Air.local>
2021-01-09 13:03:36 +0900

    restart as chattyuri

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+import SignIn from './SignIn';
+
+export default () => {
+  const [name, setName]= useState ('');
+  console.log({name});
+
+  return <SignIn setName={setName} />;
+};
