4a7aadbc1567e3b15aa65943e458423385310815
Yuri Saito <yuri@Yuris-MacBook-Air.local>
2021-01-09 14:00:07 +0900

    create config

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+import Main from './Main';
+import SignIn from './SignIn';
+import config from '../config.json';
+
+export default () => {
+  const [name, setName]= useState ('');
+  console.log({name});
+
+  if(config.signInEnabled && name ===''){
+    return <SignIn setName = {setName}/>;
+  } else {
+    return <Main name={name}/>;
+  }
+
+  return <SignIn setName={setName} />;
+};
