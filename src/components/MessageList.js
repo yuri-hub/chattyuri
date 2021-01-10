d5402339c6eab5271fe931d18a52e3ae4d8b95ea
Yuri Saito <yuri@Yuris-MacBook-Air.local>
2021-01-10 11:15:24 +0900

    apply grid row

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        gridRow: 1,
+    },
+});
 
 const MessageList = () => {
-    return <div>メッセージリスト</div>;
+    const classes = useStyles();
+    return <div className={classes.root}>メッセージリスト</div>;
 };
 
 export default MessageList;
